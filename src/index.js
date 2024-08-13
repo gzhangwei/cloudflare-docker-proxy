de40065587ff1dd38528d09179f1bff0256a2b00
gzhangwei <167850149+gzhangwei@users.noreply.github.com>
2024-08-13 15:39:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.duizhang.fun": dockerHub,
+  "quay.duizhang.fun": "https://quay.io",
+  "gcr.duizhang.fun": "https://gcr.io",
+  "k8s-gcr.duizhang.fun": "https://k8s.gcr.io",
+  "k8s.duizhang.fun": "https://registry.k8s.io",
+  "ghcr.duizhang.fun": "https://ghcr.io",
+  "cloudsmith.duizhang.fun": "https://docker.cloudsmith.io",
+  "ecr.duizhang.fun": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.duizhang.fun": dockerHub,
 };
 
 function routeByHosts(host) {
